cf24c344ee113ca0eb45cba29e523a381253b68b
renaldoespindula <renaldo.espindula@portobello.com.br>
2024-05-29 20:34:52 -0300

    Remove unused code

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
  
 function Header() {
     return (
@@ -19,9 +17,6 @@ function Header() {
               aria-label="menu"
               sx={{ mr: 2 }}
             >
-              {/*  
-              <MenuIcon />
-              */}
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Gerenciamento de Projetos
